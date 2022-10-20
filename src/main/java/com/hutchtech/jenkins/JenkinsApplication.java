6efd404ca4a86ebccdc5cd46328490321e247e67
viswakartheekreddy <viswakarthikreddy@gmail.com>
2022-10-20 17:27:04 +0530

    Basic getmapping added

--- src/main/java/com/hutchtech/jenkins/JenkinsApplication.java
+++ src/main/java/com/hutchtech/jenkins/JenkinsApplication.java
@@ -2,10 +2,20 @@ package com.hutchtech.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/api")
 public class JenkinsApplication {
 
+	@GetMapping("/")
+	public String show() {
+		return "Hello World";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsApplication.class, args);
 	}
